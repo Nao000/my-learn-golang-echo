9a75eb0c90c690f99616414be77878e1f4692f2d
nao000 <nao000dotcom@gmail.com>
2023-04-22 19:28:13 +0900

    HTML を出力してみる #1
    https://github.com/Nao000/my-learn-golang-echo/issues/1
    
    /hello でアクセスできる

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my-learn-golang-echo
 個人的なGo言語とEchoの勉強用リポジトリです
+
+echo guide
+https://echo.labstack.com/guide/
--- ubuntu/echo/main.go
+++ ubuntu/echo/main.go
@@ -3,10 +3,33 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"echo/controller"
+
+	"io" // template で必要
+	"net/http" // template で必要
+	"text/template" // template で必要
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+
 	e := echo.New()
+	e.Renderer = t
 	e.GET("/", controller.Index)
+	e.GET("/hello", Hello)
 	e.Logger.Fatal(e.Start(":80"))
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "WorldAAAAAAAAAA")
 }
